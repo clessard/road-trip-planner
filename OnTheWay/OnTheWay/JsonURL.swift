4dafad9d953fd5d802d17cebef1bb5c6c037fe18
Ellen Seidel <eseidel@WL-203-244.WPA.Claremont.Edu>
2015-10-31 14:27:11 -0700

    added JsonURL class

---
+++ OnTheWay/OnTheWay/JsonURL.swift
@@ -0,0 +1,47 @@
+//
+//  JsonURL.swift
+//  OnTheWay
+//
+//  Created by Ellen Seidel on 10/31/15.
+//  Copyright © 2015 Carli Lessard. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public class JsonURL {
+    
+    private var url: NSString
+    
+    public init(url: NSString)
+    {
+        self.url = url
+    }
+    
+    //returns an NSURL from url that can be used to get a JSON file
+    private func getJSONURL() -> NSURL
+    {
+        let urlStr : NSString = self.url.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        let searchURL : NSURL = NSURL(string: urlStr as String)!
+        return searchURL
+    }
+    
+    //gets an encrypted polyline from a JSON file
+    func getEncryptedPolyline() -> JSON
+    {
+        let jsonRequest: NSURL = getJSONURL()
+        let jsonData = NSData(contentsOfURL: jsonRequest)
+        let json = JSON(data: jsonData!)
+        let polyline = json["routes"][0]["overview_polyline"]["points"]
+        return polyline
+    }
+    
+    //sets the lat and lng values passed in based on the lat and lng value in the JSON file associated with url
+    func setLatandLong(inout lat: Double, inout lng: Double) {
+        let jsonRequest: NSURL = getJSONURL()
+        let jsonData = NSData(contentsOfURL: jsonRequest)
+        let json = JSON(data: jsonData!)
+        lat = (json["results"][0]["geometry"]["location"]["lat"]).doubleValue
+        lng = (json["results"][0]["geometry"]["location"]["lng"]).doubleValue
+    }
+}
