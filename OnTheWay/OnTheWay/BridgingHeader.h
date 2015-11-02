b5f4153d9b7d216198508eeac5fe425e1b0f8ae9
Ellen Seidel <eseidel@WL-203-244.WPA.Claremont.Edu>
2015-11-01 22:18:44 -0800

    added new apple maps panel to the storyboard

---
+++ OnTheWay/OnTheWay/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  OnTheWay
+//
+//  Created by Ellen Seidel on 10/24/15.
+//  Copyright © 2015 Carli Lessard. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <GoogleMaps/GoogleMaps.h>
+
+#endif /* BridgingHeader_h */
