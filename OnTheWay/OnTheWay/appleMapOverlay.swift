d1a18662d36bfd00e5ad20df330448d7841858fa
Ellen Seidel <eseidel@WL-211-21.WPA.Claremont.Edu>
2015-11-09 23:13:57 -0800

    added changes to the storyboard that incorporate new navigation features

--- OnTheWay/OnTheWay/appleMapOverlay.swift
+++ OnTheWay/OnTheWay/appleMapOverlay.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import UIKit
+import MapKit
+import GoogleMaps
+
+/*
+class appleMapOverlay
+{
+    init() {
+        //do nothing
+    }
+    func addRoute(directionsPath: GMSPath) {
+        let length = directionsPath.count()
+        var pointsInLine: [CLLocationCoordinate2D] = []
+        
+        
+        var i: UInt = 0
+        for i = 0; i < length; ++i
+        {
+            print(directionsPath)
+            print("hit index loop")
+            print(directionsPath.coordinateAtIndex(i))
+            pointsInLine.append(directionsPath.coordinateAtIndex(i))
+        }
+        let polyline = MKPolyline(coordinates: &pointsInLine, count: Int(length))
+        mapView.addOverlay(polyline)
+    }
+}
+*/
+
